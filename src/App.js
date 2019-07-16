8029707e7aa9a4f8d7903f881cfc1021a7f318a1
Sammy Barasa <barsamms@gmail.com>
2019-07-16 11:18:54 +0300

    Added handler for image uploads

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import {Admin, Resource, ListGuesser, EditGuesser} from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 
-import {Hotel} from "./components/Hotel";
+import {HotelList, HotelEdit} from "./components/Hotel";
 
 import {HotelBookingList, HotelBookingEdit, HotelBookingCreate} from "./components/HotelBooking";
 
@@ -17,21 +17,36 @@ import PostIcon from '@material-ui/icons/Book';
 import UserIcon from '@material-ui/icons/Group';
 
 import Dashboard from './components/Dashboard'
+import addUploadFeature from "./components/addUploadFeature";
 
 const dataProvider = jsonServerProvider('http://127.0.0.1:8000/api');
+const uploadCapableProvider = addUploadFeature(dataProvider);
 
 const App = () =>
-    <Admin dashboard={Dashboard} dataProvider={dataProvider}>
-        <Resource name="hotels" icon={PostIcon} list={Hotel}/>
+    <Admin dashboard={Dashboard} dataProvider={uploadCapableProvider}>
+        <Resource name="hotels" icon={UserIcon}
+                  list={HotelList}
+                  edit={HotelEdit}/>
 
-        <Resource name="hotel-bookings" icon={PostIcon} list={HotelBookingList} edit={HotelBookingEdit}
+        <Resource name="hotel-bookings" icon={PostIcon}
+                  list={HotelBookingList}
+                  edit={HotelBookingEdit}
                   create={HotelBookingCreate}/>
 
-        <Resource name="hotel-rooms" list={HotelRoomList} edit={HotelRoomEdit} create={HotelRoomCreate}/>
+        <Resource name="hotel-rooms"
+                  list={HotelRoomList}
+                  edit={HotelRoomEdit}
+                  create={HotelRoomCreate}/>
 
-        <Resource name="room-types" icon={PostIcon} list={RoomTypeList} edit={RoomTypeEdit} create={RoomTypeCreate}/>
+        <Resource name="room-types" icon={PostIcon}
+                  list={RoomTypeList}
+                  edit={RoomTypeEdit}
+                  create={RoomTypeCreate}/>
 
-        <Resource name="room-type-prices" icon={PostIcon} list={RoomTypePriceList} edit={RoomTypePriceEdit} create={RoomTypePriceCreate}/>
+        <Resource name="room-type-prices" icon={PostIcon}
+                  list={RoomTypePriceList}
+                  edit={RoomTypePriceEdit}
+                  create={RoomTypePriceCreate}/>
     </Admin>
 
 
--- src/components/Hotel.js
+++ src/components/Hotel.js
@@ -1,21 +1,53 @@
 import React from 'react';
 
-import {List, Datagrid, TextField, DateField, EmailField, ImageField, EditButton} from 'react-admin';
+import {
+    List,
+    Datagrid,
+    TextField,
+    DateField,
+    EmailField,
+    ImageField,
+    EditButton,
+    DateInput,
+    TextInput,
+    SimpleForm,
+    Edit,
+    Create,
+    ImageInput
+} from 'react-admin';
 
-export const Hotel = props => (
+export const HotelList = props => (
     <List {...props}>
         <Datagrid rowClick="edit">
             <TextField source="id"/>
             <TextField source="name"/>
-            <TextField source="address"/>
-            <TextField source="city"/>
-            <TextField source="state"/>
-            <TextField source="country"/>
-            <DateField source="zip_code"/>
-            <TextField source="phone_number"/>
-            <EmailField source="email"/>
-            <ImageField source="image"/>
+            {/*<TextField source="address"/>*/}
+            {/*<TextField source="city"/>*/}
+            {/*<TextField source="state"/>*/}
+            {/*<TextField source="country"/>*/}
+            {/*<DateField source="zip_code"/>*/}
+            {/*<TextField source="phone_number"/>*/}
+            {/*<EmailField source="email"/>*/}
+            {/*<ImageField source="image"/>*/}
             <EditButton/>
         </Datagrid>
     </List>
+);
+
+export const HotelEdit = props => (
+    <Edit {...props}>
+        <SimpleForm>
+            <TextInput source="name"/>
+            {/*<DateInput source="address"/>*/}
+            {/*<TextInput source="city"/>*/}
+            {/*<TextInput source="state"/>*/}
+            {/*<TextInput source="country"/>*/}
+            {/*<DateInput source="zip_code"/>*/}
+            {/*<TextInput source="phone_number"/>*/}
+            {/*<TextInput source="email"/>*/}
+            <ImageInput source="pictures" label="Related pictures" accept="image/*" multiple={false}>
+                <ImageField source="src" title="title" />
+            </ImageInput>
+        </SimpleForm>
+    </Edit>
 );
